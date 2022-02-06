7c066fe9ab9f63d1260208b02171bdb2db3e747a
Julian Büttner <buettner-julian@t-online.de>
2022-02-06 22:20:50 +0100

    Fix prometheus metrics error v2

--- src/api.rs
+++ src/api.rs
@@ -140,7 +140,7 @@ pub async fn get_metrics(domainconfig: &State<DomainConfig>) -> Result<String, A
                 let units = a.get_units();
                 let unit_price = a.get_unit_price();
                 result.push_str(&format!(
-                    "get-rich-slow {{fund=\"{}\", name=\"{}\", description=\"{}\", growth={} }} {}\n",
+                    "get_rich_slow {{fund=\"{}\", name=\"{}\", description=\"{}\", growth={} }} {}\n",
                     fund.name, a.get_name(), a.get_description(), a.get_growth().get_real_growth(), units * unit_price,
                 ));
             }
